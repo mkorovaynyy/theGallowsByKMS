8c12127b263b31dbffc8ba93cc88cf8c551170c6
progTD <corowainy@yandex.ru>
2025-03-23 17:09:51 +0300

    + WordRandomiser

--- src/main/java/org/example/service/WordRandomiser.java
+++ src/main/java/org/example/service/WordRandomiser.java
@@ -1,13 +1,18 @@
 package org.example.service;
 
+import org.example.exception.FileNotFoundExceptionInZip;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.Properties;
+import java.util.Random;
 
 public class WordRandomiser {
     private final Dictionary dictionary;
+    private final Random random;
 
-    public WordRandomiser(String configFilePath) throws IOException {
+    public WordRandomiser(String configFilePath) throws IOException, FileNotFoundExceptionInZip {
         Properties properties = new Properties();
         try (InputStream input = getClass().getClassLoader().getResourceAsStream(configFilePath)) {
             if (input == null) {
@@ -16,9 +21,15 @@ public class WordRandomiser {
             properties.load(input);
         }
         this.dictionary = new Dictionary(configFilePath);
+        this.random = new Random(dictionary.getDictionary().size()-1);
     }
 
     public Dictionary getDictionary() {
         return dictionary;
+    }
+
+    public String getRandomWord() throws FileNotFoundExceptionInZip, IOException {
+        List<String> allWords = dictionary.getDictionary();
+        return allWords.get(random.nextInt());
     }
 }
